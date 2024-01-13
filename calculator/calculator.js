4e8b206d427f0b3c0548ec49ebd6a7bda65befd2
nahilnasir <156524095+nahilnasir@users.noreply.github.com>
2024-01-13 19:51:55 +0530

    Add files via upload
    simple calculator made

---
+++ calculator/calculator.js
@@ -0,0 +1,43 @@
+const displayValue = document.getElementById('inputValuedisplay');
+function insertvalue(value) {
+    playButtonClick();
+    displayValue.value += value;
+
+}
+
+function clearFully()
+{
+    playButtonClick();
+
+    displayValue.value = '';
+    var historyList = document.getElementById('calculationHistory');
+    while (historyList.firstChild) {
+        historyList.removeChild(historyList.firstChild);
+    } 
+}
+function calculate(){
+    try {
+        var result = eval(displayValue.value);
+        var historyList = document.getElementById('calculationHistory');
+        var listItem = document.createElement('li');
+        listItem.textContent = `${document.getElementById('inputValuedisplay').value} = ${result}`;
+        historyList.appendChild(listItem);
+        displayValue.value = result;
+    } catch (error) {
+       displayValue.value = 'Error';
+    }
+}
+function elementremoveOne(){
+    displayValue.value = displayValue.value.slice(0,-1);
+    playButtonClick();
+}
+function playButtonClick() {
+     
+    
+    var buttonClickSound = document.getElementById('buttonClickSound');
+    if (buttonClickSound) {
+        buttonClickSound.currentTime = 0;  
+        buttonClickSound.playbackRate = 2;
+        buttonClickSound.play();
+    }
+}
